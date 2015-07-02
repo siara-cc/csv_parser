a76c58c87bfc9249ade5ac9e2ecb4eb44e2d58c2
siara-cc <arun@siara.cc>
2015-07-02 13:46:35 +0530

    Suport java.io.Reader

--- src/cc/siara/csv/CSVParser.java
+++ src/cc/siara/csv/CSVParser.java
@@ -1,13 +1,102 @@
+/*
+ * Copyright (C) 2015 arun@siara.cc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author Arundale R.
+ *
+ */
 package cc.siara.csv;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class CSVParser {
 
+   final byte ST_NOT_STARTED = 0;
+   final byte ST_DATA_STARTED_WITHOUT_QUOTE = 1;
+   final byte ST_DATA_STARTED_WITH_QUOTE = 2;
+   final byte ST_QUOTE_WITHIN_QUOTE = 3;
+   final byte ST_DATA_ENDED_WITH_QUOTE = 4;
+   final byte ST_FIELD_ENDED = 5;
+
    int line_no = 1;
    int col_no = 1;
-   int save_line_no;
-   int save_col_no;
+   boolean isWithinComment = false;
+   boolean isEOL = false;
+   boolean isEOS = false;
+   byte state = ST_NOT_STARTED;
+   ExceptionHandler ex;
+   StringBuffer data = new StringBuffer();
+   StringBuffer backlog = new StringBuffer();
+   String reinsertedToken = null;
+   String lastToken = null;
+   int reinsertedChar = -1;
+   int lastChar = -1;
+   int max_value_len = 65535;
 
-   private static String encodeToCSVText(String value) {
+   public CSVParser(ExceptionHandler e) {
+       ex = e;
+       reset();
+   }
+
+   private void increment_counters(char c) {
+      if (c == '\n') {
+         this.line_no++;
+         this.col_no = 1;
+      } else
+      this.col_no++;
+   }
+
+   private void decrement_counters(char c) {
+      if (c == '\n') {
+         this.line_no--;
+         this.col_no = 1;
+      } else
+         this.col_no--;
+   }
+
+   private void reset_counters() {
+      this.line_no = 1;
+      this.col_no = 1;
+   }
+
+   private boolean checkEOF(int c, StringBuffer data) {
+      if (c == ',') {
+         state = ST_FIELD_ENDED;
+         isEOL = false;
+      } else
+      if (c == '\n') {
+         state = ST_FIELD_ENDED;
+         isEOL = true;
+         int lastPos = data.length()-1;
+         if (lastPos > -1 && data.charAt(lastPos) == '\r')
+            data.setLength(lastPos);
+      } else
+         return false;
+      return true;
+   }
+
+   private String windUp(StringBuffer data) {
+        isEOS = true;
+        isEOL = true;
+        state = ST_NOT_STARTED;
+        return data.toString();
+   }
+
+   public static String encodeToCSVText(String value) {
       if (value == null)
          return value;
       if (value.indexOf(',') != -1 || value.indexOf('\n') != -1
@@ -19,170 +108,169 @@ public class CSVParser {
       return value;
    }
 
-   private void increment_counters(char c) {
-      if (c == '\n') {
-         this.line_no++;
-         this.col_no = 1;
-      } else
-      this.col_no++;
+   public void reset() {
+       state = ST_NOT_STARTED;
+       reset_counters();
    }
 
-   private void save_counters() {
-      this.save_line_no = this.line_no;
-      this.save_col_no = this.col_no;
-   }
-
-   private void restore_counters() {
-      this.line_no = this.save_line_no;
-      this.col_no = this.save_col_no;
-   }
-
-   public int skip_box_comment(String input, int i, int isize, boolean looking_for_quote, boolean to_increment_counters) {
-      boolean box_started = false;
-      int j;
-      for (j=i; j<isize; j++) {
-         char c = input.charAt(j);
-         if (j < (isize-1)) {
-            if (c == '/' && input.charAt(j+1) == '*') {
-               box_started = true;
-               j++;
-               if (to_increment_counters) {
-                  this.increment_counters(c);
-                  this.increment_counters(c);
-               }
-               continue;
-            }
-            if (c == '*' && input.charAt(j+1) == '/') {
-               box_started = false;
-               j++;
-               if (to_increment_counters) {
-                  this.increment_counters(c);
-                  this.increment_counters(c);
-               }
-               continue;
-            }
-         }
-         if (!box_started) {
-            if (looking_for_quote) {
-               if (c != ' ' && c != '\t')
-                  break;
-               if (to_increment_counters)
-                  this.increment_counters(c);
-            } else
-               break;
-         }
-      }
-      return j;
-   }
-
-   public boolean is_eol(String input, int start, int end) {
-      int len = end-start-1;
-      if (len >= 0 && input.charAt(end-1) == '\n')
-         return true;
-      if (input.length() == (end-1))
-         return true;
-      return false;
-   }
-
-   public String get_token_value(String input, int start, int end) {
-      String ret = "";
-      boolean is_quote_enclosed = false;
-      int i = start;
-      boolean is_quote_end = false;
-      for (; i<end; i++) {
-         i = this.skip_box_comment(input, i, end, true, false);
-         char c = input.charAt(i++);
-         if (c == '"')
-            is_quote_enclosed = true;
-         else
-            i = start;
-         break;
-      }
-      for (; i<end; i++) {
-         char c = input.charAt(i);
-         if (is_quote_end) {
-            i = this.skip_box_comment(input, i, end, true, false);
-            c = input.charAt(i);
-            if (c == ',' || c == '\n')
-               break;
-            if (c != ',' && c != ' ' && c != '\t')
-               is_quote_end = false;
-         }
-         if (is_quote_enclosed) {
-            if (c == '"') {
-               if (i == end-1) break;
-               char c_next = input.charAt(i+1);
-               if (c_next == '"') {
-                  ret += c;
-                  i++;
-                  continue;
-               }
-               is_quote_end = true;
-               continue;
-            }
-         } else {
-            i = this.skip_box_comment(input, i, end, false, false);
-            c = input.charAt(i);
-            if (c == ',' || c == '\n')
-               break;
-         }
-         ret += c;
+   public int readChar(Reader r) throws IOException {
+       int i;
+       try {
+          i = r.read();
+       } catch (IOException e) {
+          if (ex != null)
+             ex.set_err(ExceptionHandler.E_IO, this);
+          throw e;
        }
+       lastChar = i;
+       return i;
+   }
+
+    public void processChar(int i) {
+        char c = (char) (i & 0xFFFF);
+        if (isWithinComment) {
+           increment_counters(c);
+           return;
+        }
+        switch (state) {
+        case ST_NOT_STARTED:
+            if (c == ' ' || c == '\t') {
+                backlog.append(c);
+            } else if (checkEOF(c, data)) {
+            } else if (c == '"') {
+                state = ST_DATA_STARTED_WITH_QUOTE;
+                backlog.setLength(0);
+            } else {
+                state = ST_DATA_STARTED_WITHOUT_QUOTE;
+                if (backlog.length() > 0) {
+                    data.append(backlog);
+                    backlog.setLength(0);
+                }
+                data.append(c);
+            }
+            break;
+        case ST_DATA_STARTED_WITHOUT_QUOTE:
+            if (checkEOF(c, data)) {
+            } else
+                data.append(c);
+            break;
+        case ST_DATA_STARTED_WITH_QUOTE:
+            if (c == '"')
+                state = ST_QUOTE_WITHIN_QUOTE;
+            else
+                data.append(c);
+            break;
+        case ST_QUOTE_WITHIN_QUOTE:
+            if (c == '"') {
+                state = ST_DATA_STARTED_WITH_QUOTE;
+                data.append(c);
+            } else if (checkEOF(c, data)) {
+            } else if (c == ' ' || c == '\t' || c == '\r') {
+                state = ST_DATA_ENDED_WITH_QUOTE;
+            } else {
+                state = ST_DATA_STARTED_WITH_QUOTE;
+                ex.add_warn(ExceptionHandler.W_CHAR_INVALID, this);
+                data.append(c);
+            }
+            break;
+        case ST_DATA_ENDED_WITH_QUOTE:
+            if (c == ' ' || c == '\t') {
+            } else if (checkEOF(c, data)) {
+            } else
+                ex.add_warn(ExceptionHandler.W_CHAR_INVALID, this);
+            break;
+        }
+        increment_counters(c);
+    }
+
+   public void reinsertLastToken() {
+       reinsertedToken = lastToken;
+   }
+
+   public void reInsertLastChar() {
+       reinsertedChar = lastChar;
+       decrement_counters((char)lastChar);
+   }
+
+   public String parseNextToken(Reader r) throws IOException {
+       if (reinsertedToken != null) {
+           String ret = reinsertedToken;
+           reinsertedToken = null;
+           return ret;
+       }
+       if (state == ST_FIELD_ENDED) {
+           isEOL = false;
+           state = ST_NOT_STARTED;
+           backlog.setLength(0);
+       }
+       int c = reinsertedChar;
+       if (c == -1)
+          c = readChar(r);
+       else
+          reinsertedChar = -1;
+       while (state != ST_FIELD_ENDED) {
+           if (c == -1)
+              return windUp(data);
+           if (c == '/' && !isWithinComment &&
+                   state != ST_DATA_STARTED_WITH_QUOTE) {
+              int c_next = readChar(r);
+              if (c_next == -1) {
+                 processChar(c);
+                 return windUp(data);
+              }
+              if (c_next == '*')
+                  isWithinComment = true;
+              else {
+                  processChar(c);
+                  processChar(c_next);
+              }
+           } else
+           if (c == '*' && isWithinComment &&
+                 state != ST_DATA_STARTED_WITH_QUOTE) {
+              int c_next = readChar(r);
+              if (c_next == -1) {
+                 processChar(c);
+                 return windUp(data);
+              }
+              if (c_next == '/')
+                  isWithinComment = false;
+              else {
+                  processChar(c);
+                  processChar(c_next);
+              }
+           } else
+              processChar(c);
+          if (state != ST_FIELD_ENDED)
+             c = readChar(r);
+       }
+       String ret = data.toString();
+       data.setLength(0);
+       lastToken = ret;
        return ret;
    }
 
-   int find_next_token_pos(String input, int isize, int prev_pos, ExceptionHandler ex) {
-       int i = prev_pos;
-       boolean is_quote_end = false;
-       boolean is_quote_started = false;
-       if (prev_pos == 0) {
-          this.line_no = 1;
-          this.col_no = 1;
-       }
-       this.save_counters();
-       for (; i<isize; i++) {
-          i = this.skip_box_comment(input, i, isize, true, true);
-          char c = input.charAt(i++); this.increment_counters(c);
-          if (c == '"')
-             is_quote_started = true;
-          else {
-             i = prev_pos;
-             this.restore_counters();
-          }
-          break;
-       }
-       for (; i<isize; i++) {
-          char c = input.charAt(i);
-          if (is_quote_end) {
-             i = this.skip_box_comment(input, i, isize, false, true);
-             c = input.charAt(i); this.increment_counters(c);
-             if (c == ',' || c == '\n') break;
-             if (c != ',' && c != ' ' && c != '\t') {
-                ex.add_warn(6, this);
-                is_quote_end = false;
-             }
-          }
-          if (is_quote_started) {
-             if (c == '"') {
-                if (i == isize-1) break;
-                char c_next = input.charAt(i+1);
-                if (c_next == '"') {
-                   i++;
-                   this.increment_counters(c_next);
-                   continue;
-                }
-                is_quote_end = true;
-                continue;
-             }
-             this.increment_counters(c);
-          } else {
-             i = this.skip_box_comment(input, i, isize, false, true);
-             c = input.charAt(i); this.increment_counters(c);
-             if (c == ',' || c == '\n')
-                break;
-          }
-       } 
-       return i+1;
-   }
+    public boolean isEOL() {
+        return isEOL;
+    }
+    
+    public boolean isEOS() {
+        return isEOS;
+    }
+
+    public int getLineNo() {
+        return line_no;
+    }
+
+    public void setLineNo(int line_no) {
+        this.line_no = line_no;
+    }
+
+    public int getColNo() {
+        return col_no;
+    }
+
+    public void setColNo(int col_no) {
+        this.col_no = col_no;
+    }
 
 }
--- src/cc/siara/csv/ExceptionHandler.java
+++ src/cc/siara/csv/ExceptionHandler.java
@@ -1,32 +1,75 @@
+/*
+ * Copyright (C) 2015 arun@siara.cc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author Arundale R.
+ *
+ */
 package cc.siara.csv;
 
-import java.util.*;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ExceptionHandler {
 
     String lang = "en-US";
-    int error_code = 0;
-    int err_line_no = 0;
-    int err_col_no = 0;
+    public short error_code;
+    public int err_line_no;
+    public int err_col_no;
 
-    List<Integer> warning_codes = new LinkedList<Integer>();
-    List<Integer> warning_line_nos = new LinkedList<Integer>();
-    List<Integer> warning_col_nos = new LinkedList<Integer>();
-    List<Integer> validation_codes = new LinkedList<Integer>();
-    List<Integer> validation_line_nos = new LinkedList<Integer>();
-    List<Integer> validation_col_nos = new LinkedList<Integer>();
+    public List<Short> warning_codes;
+    public List<Integer> warning_line_nos;
+    public List<Integer> warning_col_nos;
+    public List<Short> validation_codes;
+    public List<Integer> validation_line_nos;
+    public List<Integer> validation_col_nos;
     Hashtable<String, String[]> msgs = new Hashtable<String, String[]>();
 
-    ExceptionHandler() {
+    public static final short E_IO = 1;
+    public static final short W_CHAR_INVALID = 2;
+    public static final short E_SCH_START_WITH_SPACE = 3;
+    public static final short E_DUPLICATE_NODE = 4;
+    public static final short E_DOWN_2_LEVELS = 5;
+    public static final short E_TOO_MANY_CHARS = 6;
+    public static final short E_NODE_NOT_FOUND = 7;
+    public static final short E_ONLY_ONE_ROOT = 8;
+
+    public ExceptionHandler() {
        msgs.put("en-US", new String[] {""
+        , "IOException"
+        , "Unexpected character"
         , "Schema definition cannot begin with a space"
         , "Duplicate node definition"
         , "Cannot go down two levels"
         , "Too many characters in a column"
         , "Node not found"
-        , "Improperly closed quote"
         , "There can be only one root node"
         });
+       reset_exceptions();
+    }
+
+    public void reset_exceptions() {
+        error_code = 0;
+        err_line_no = 0;
+        err_col_no = 0;
+        warning_codes = new LinkedList<Short>();
+        warning_line_nos = new LinkedList<Integer>();
+        warning_col_nos = new LinkedList<Integer>();
+        validation_codes = new LinkedList<Short>();
+        validation_line_nos = new LinkedList<Integer>();
+        validation_col_nos = new LinkedList<Integer>();
     }
 
     public String get_error_message() {
@@ -36,62 +79,63 @@ public class ExceptionHandler {
                 + msgs.get(lang)[error_code];
     }
 
-	public String get_warn_messages() {
-		if (warning_codes.size() == 0)
-			return "";
-		StringBuffer warn_msgs = new StringBuffer();
-		for (int i = 0; i < this.warning_codes.size(); i++) {
-			if (i > 0)
-				warn_msgs.append("\r\n");
-			warn_msgs.append("Line:").append(this.warning_line_nos.get(i))
-					.append(", Col:").append(this.warning_col_nos.get(i))
-					.append(": ")
-					.append(msgs.get(lang)[this.warning_codes.get(i)]);
-		}
-		return warn_msgs.toString();
-	}
+    public String get_warn_messages() {
+        if (warning_codes.size() == 0)
+            return "";
+        StringBuffer warn_msgs = new StringBuffer();
+        for (int i = 0; i < this.warning_codes.size(); i++) {
+            if (i > 0)
+                warn_msgs.append("\r\n");
+            warn_msgs.append("Line:").append(this.warning_line_nos.get(i))
+                    .append(", Col:").append(this.warning_col_nos.get(i))
+                    .append(": ")
+                    .append(msgs.get(lang)[this.warning_codes.get(i)]);
+        }
+        return warn_msgs.toString();
+    }
 
-	public String get_val_messages() {
-		if (this.validation_codes.size() == 0)
-			return "";
-		StringBuffer val_msgs = new StringBuffer();
-		for (int i = 0; i < this.validation_codes.size(); i++) {
-			if (i > 0)
-				val_msgs.append("\r\n");
-			val_msgs.append("Line:").append(this.validation_line_nos.get(i))
-					.append(", Col:").append(this.validation_col_nos.get(i))
-					.append(": ")
-					.append(msgs.get(lang)[this.validation_codes.get(i)]);
-		}
-		return val_msgs.toString();
-	}
+    public String get_val_messages() {
+        if (this.validation_codes.size() == 0)
+            return "";
+        StringBuffer val_msgs = new StringBuffer();
+        for (int i = 0; i < this.validation_codes.size(); i++) {
+            if (i > 0)
+                val_msgs.append("\r\n");
+            val_msgs.append("Line:").append(this.validation_line_nos.get(i))
+                    .append(", Col:").append(this.validation_col_nos.get(i))
+                    .append(": ")
+                    .append(msgs.get(lang)[this.validation_codes.get(i)]);
+        }
+        return val_msgs.toString();
+    }
 
-	public void add_err(int err_code, CSVParser csv_parser) {
-		this.error_code = err_code;
-		this.err_line_no = csv_parser.line_no;
-		this.err_col_no = csv_parser.col_no;
-	}
+    public void set_err(short err_code, CSVParser csv_parser) {
+        this.error_code = err_code;
+        this.err_line_no = csv_parser.line_no;
+        this.err_col_no = csv_parser.col_no;
+    }
 
-	public void add_warn(int warn_code, CSVParser csv_parser) {
-		this.warning_codes.add(warn_code);
-		this.warning_line_nos.add(csv_parser.line_no);
-		this.warning_col_nos.add(csv_parser.col_no);
-	}
+    public void add_warn(short warn_code, CSVParser csv_parser) {
+        this.warning_codes.add(warn_code);
+        this.warning_line_nos.add(csv_parser.line_no);
+        this.warning_col_nos.add(csv_parser.col_no);
+    }
 
-	public void add_val_err(int val_code, CSVParser csv_parser) {
-		this.validation_codes.add(val_code);
-		this.validation_line_nos.add(csv_parser.line_no);
-		this.validation_col_nos.add(csv_parser.col_no);
-	}
-	/*    public String display_exceptions = function() {
-   var err_msg = this.get_error_message();
-   if (err_msg != "") alert("Error:\n"+err_msg);
-   var warn_msg = this.get_warn_messages();
-   if (warn_msg != "") alert("Warning(s):\n"+warn_msg);
-   var val_msg = this.get_val_messages();
-   if (val_msg != "") alert("Validation Error(s):\n"+val_msg);
-   if (err_msg != "") return true;
-   return false;
-}*/
+    public void add_val_err(short val_code, CSVParser csv_parser) {
+        this.validation_codes.add(val_code);
+        this.validation_line_nos.add(csv_parser.line_no);
+        this.validation_col_nos.add(csv_parser.col_no);
+    }
+
+    public String get_all_exceptions() {
+       StringBuffer ex_str = new StringBuffer();
+       String err_msg = this.get_error_message();
+       if (err_msg != "") ex_str.append("Error:\n").append(err_msg);
+       String warn_msg = this.get_warn_messages();
+       if (warn_msg != "") ex_str.append("Warning(s):\n").append(warn_msg);
+       String val_msg = this.get_val_messages();
+       if (val_msg != "") ex_str.append("Validation Error(s):\n").append(val_msg);
+       return ex_str.toString();
+    }
 
 }
